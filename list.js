c87e91aa78a4faa50d4db62cbf44472103f6ae1b
Julio Augusto <julioams@icloud.com>
2019-07-13 20:27:20 -0300

    add app name

--- list.js
+++ list.js
@@ -1,29 +1,34 @@
 export const LIST = {
   "nubank": { 
+    appName: "Nubank",
     packageName: "com.nu.production", 
     urlSheme: "nuapp", 
     urlParams: "",
     type: "finance",
   },
   "bb": { 
+    appName: "Bando do Brasil",
     packageName: "br.com.bb.android", 
     urlSheme: "bbapp", 
     urlParams: "",
     type: "finance",
   },
   "bradesco": { 
+    appName: "Bradesco",
     packageName: "com.bradesco", 
     urlSheme: "BDiPhoneVarejo", 
     urlParams: "",
     type: "finance",
   },
   "itau": { 
+    appName: "Itaú",
     packageName: "com.itau", 
     urlSheme: "itauvarejo", 
     urlParams: "",
     type: "finance",
   },
   "santader": { 
+    appName: "Santader",
     packageName: "com.santander.app", 
     urlSheme: "santanderpf", 
     urlParams: "",
