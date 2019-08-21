d4d28a2dc4ccafadff6d2ede6cb18eede95b8146
Julio Augusto <julioams@icloud.com>
2019-08-21 19:30:50 -0300

    add new apps

--- list.js
+++ list.js
@@ -1,44 +1,79 @@
 export const LIST = {
-  "nubank": { 
+  "nubank": {
     appName: "Nubank",
-    packageName: "com.nu.production", 
-    urlScheme: "nuapp", 
+    packageName: "com.nu.production",
+    urlScheme: "nuapp",
     urlParams: "",
     type: "finance",
   },
-  "bb": { 
+  "bb": {
     appName: "Bando do Brasil",
-    packageName: "br.com.bb.android", 
-    urlScheme: "bbapp", 
+    packageName: "br.com.bb.android",
+    urlScheme: "bbapp",
     urlParams: "",
     type: "finance",
   },
-  "bradesco": { 
+  "bradesco": {
     appName: "Bradesco",
-    packageName: "com.bradesco", 
-    urlScheme: "BDiPhoneVarejo", 
+    packageName: "com.bradesco",
+    urlScheme: "BDiPhoneVarejo",
     urlParams: "",
     type: "finance",
   },
-  "itau": { 
+  "itau": {
     appName: "Itaú",
-    packageName: "com.itau", 
-    urlScheme: "itauvarejo", 
+    packageName: "com.itau",
+    urlScheme: "itauvarejo",
     urlParams: "",
     type: "finance",
   },
-  "santader": { 
+  "santader": {
     appName: "Santader",
-    packageName: "com.santander.app", 
-    urlScheme: "santanderpf", 
+    packageName: "com.santander.app",
+    urlScheme: "santanderpf",
     urlParams: "",
     type: "finance",
   },
-  "maps": { 
+  "next": {
+    appName: "Next",
+    packageName: "br.com.bradesco.next",
+    urlScheme: "next",
+    urlParams: "",
+    type: "finance"
+  },
+  "maps": {
     appName: "Maps",
-    packageName: "", 
-    urlScheme: "maps", 
+    packageName: "",
+    urlScheme: "maps",
     urlParams: "",
     type: "Navigation",
   },
+  "uber": {
+    appName: "Uber",
+    packageName: "com.ubercab",
+    urlScheme: "uber",
+    urlParams: "",
+    type: "Navigation"
+  },
+  "99": {
+    appName: "99",
+    packageName: "com.taxis99",
+    urlScheme: "taxis99",
+    urlParams: "",
+    type: "Navigation"
+  },
+  "waze": {
+    appName: "Waze",
+    packageName: "com.waze",
+    urlScheme: "waze",
+    urlParams: "",
+    type: "Navigation"
+  },
+  "googleMaps": {
+    appName: "Google Maps",
+    packageName: "com.google.android.apps.maps",
+    urlScheme: "comgooglemaps",
+    urlParams: "?center=40.765819,-73.975866&zoom=14&views=traffic",
+    type: "Navigation"
+  }
 };
